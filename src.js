4559d42724bc01ab8d1b576e72234c791ab4ad59
MugiSus <42643211+MugiSus@users.noreply.github.com>
2022-03-19 00:09:19 +0900

    commit

---
+++ src.js
@@ -0,0 +1,15 @@
+const elems = {};
+[
+    elems.ax, elems.ay, elems.az,
+    elems.sx, elems.sy, elems.sz,
+    elems.dx, elems.dy, elems.dz,
+    elems.ox, elems.oy, elems.oz,
+    elems.tx, elems.ty, elems.tz,
+] = document.querySelectorAll(".values-container .values");
+
+window.addEventListener("devicemotion", event => {
+    const {acceleration} = event.acceleration;
+    elems.ax.textContent = acceleration.x.toFixed(2);
+    elems.ay.textContent = acceleration.y.toFixed(2);
+    elems.az.textContent = acceleration.z.toFixed(2);
+})
