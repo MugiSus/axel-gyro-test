aeab6386840d21f04b357e12d320f6770d89b662
MugiSus <42643211+MugiSus@users.noreply.github.com>
2022-03-19 19:46:30 +0900

    commit

--- src.js
+++ src.js
@@ -1,12 +1,17 @@
 const elems = {};
 [
     elems.interval,
+    elems.at,
     elems.ax, elems.ay, elems.az,
     elems.sx, elems.sy, elems.sz,
     elems.dx, elems.dy, elems.dz,
+    elems.ot,
     elems.ox, elems.oy, elems.oz,
     elems.tx, elems.ty, elems.tz,
 ] = document.querySelectorAll(".values-container .values");
+
+const accelerationThreshold = 1;
+const rotationRateThreshold = 0.8;
 
 const speed = {x: 0, y: 0, z: 0};
 const displacement = {x: 0, y: 0, z: 0};
@@ -22,20 +27,22 @@ window.addEventListener("devicemotion", event => {
 
     if (intervalSeconds > 0.1) return;
 
-    speed.x += acceleration.x * intervalSeconds;
-    speed.y += acceleration.y * intervalSeconds;
-    speed.z += acceleration.z * intervalSeconds;
+    if (Math.abs(acceleration.x) > accelerationThreshold) speed.x += acceleration.x * intervalSeconds;
+    if (Math.abs(acceleration.y) > accelerationThreshold) speed.y += acceleration.y * intervalSeconds;
+    if (Math.abs(acceleration.z) > accelerationThreshold) speed.z += acceleration.z * intervalSeconds;
     
     displacement.x += speed.x * intervalSeconds;
     displacement.y += speed.y * intervalSeconds;
     displacement.z += speed.z * intervalSeconds;
 
-    theta.x += rotationRate.alpha * intervalSeconds;
-    theta.y += rotationRate.beta * intervalSeconds;
-    theta.z += rotationRate.gamma * intervalSeconds;
+    if (Math.abs(rotationRate.alpha) > rotationRateThreshold) theta.x += rotationRate.alpha * intervalSeconds;
+    if (Math.abs(rotationRate.beta) >  rotationRateThreshold) theta.y += rotationRate.beta * intervalSeconds;
+    if (Math.abs(rotationRate.gamma) > rotationRateThreshold) theta.z += rotationRate.gamma * intervalSeconds;
 
     elems.interval.innerText = intervalSeconds.toFixed(3);
 
+    elems.at.textContent = accelerationThreshold;
+    
     elems.ax.textContent = acceleration.x.toFixed(5);
     elems.ay.textContent = acceleration.y.toFixed(5);
     elems.az.textContent = acceleration.z.toFixed(5);
@@ -43,11 +50,13 @@ window.addEventListener("devicemotion", event => {
     elems.sx.textContent = speed.x.toFixed(5);
     elems.sy.textContent = speed.y.toFixed(5);
     elems.sz.textContent = speed.z.toFixed(5);
-
+    
     elems.dx.textContent = displacement.x.toFixed(5);
     elems.dy.textContent = displacement.y.toFixed(5);
     elems.dz.textContent = displacement.z.toFixed(5);
-
+    
+    elems.ot.textContent = rotationRateThreshold;
+    
     elems.ox.textContent = rotationRate.alpha.toFixed(5);
     elems.oy.textContent = rotationRate.beta.toFixed(5);
     elems.oz.textContent = rotationRate.gamma.toFixed(5);
